359c8bbb2a5c7c52f386e095e52d114b6f08dccc
DESKTOP-JH64IUN\erayi <erayizgin@gmail.com>
2022-03-17 16:11:10 +0300

    Redux-Toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+//import * as serviceWorker from './serviceWorker'
+import { Provider } from 'react-redux'
+import store from './store/store';
+//import store from 'store/store'
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
-
+  document.getElementById('root'),
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
